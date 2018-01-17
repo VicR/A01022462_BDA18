061a1fa93714c9a860817ae20d6fbb2877d46ac0
Victor Rendon <victor.rs@eucledian.mx>
2018-01-17 15:35:00 -0600

    stored procedures class

---
+++ Actividades/A3.1.StoredProc/3storedProc.sql
@@ -0,0 +1,38 @@
+--Víctor Rendón S. | A01022462
+
+-- Muestra productos por "product line"
+delimiter $$
+create procedure show_products(
+IN linea_producto varchar(50))
+
+BEGIN
+	declare line varchar(50);
+
+	set line = concat(linea_producto, "%");
+	select * from products
+	where productLine like line;
+END$$
+delimiter ;
+
+-- Muestra cuantos clientes inicia su nombre con la letra "variable"
+delimiter $$
+create procedure show_customer_letter(
+IN letter CHAR)
+
+BEGIN
+	declare ctr int default 0;
+
+	select count(*) into ctr from customers WHERE customerName LIKE CONCAT(letter, "%");
+	select ctr;
+END$$
+delimiter ;
+
+--Encontrar más caro y más barato
+delimiter $$
+create procedure find_prices(
+)
+BEGIN
+
+	select MAX(buyPrice), MIN(buyPrice) from products;
+END$$
+delimiter ;
